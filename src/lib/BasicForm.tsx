7d9b557664bd7034b0a80d95e636b20189a1f7b6
Paul Clark <paul@sandtreader.com>
2023-06-12 16:25:52 +0100

    Use Autocomplete for searchable add list

--- src/lib/BasicForm.tsx
+++ src/lib/BasicForm.tsx
@@ -4,7 +4,7 @@ import { ReactNode, useState, useEffect } from 'react';
 
 import { Button, TextField, Stack, IconButton, Icon,
          DialogActions, DialogContent, DialogTitle, Chip, Typography,
-         Select, MenuItem
+         Select, MenuItem, Autocomplete
 } from '@mui/material';
 
 import { FormIntent, FormProps, HasUniqueId } from './Types';
@@ -132,43 +132,38 @@ export default function BasicForm<T>(
       const unusedItems = field.arrayItems?.filter(
         ai => !items.find(i => ai.id === i.id)) || [];
 
-      return <Stack direction="row" spacing={0} flexWrap="wrap"
-                    justifyContent="flex-start" alignItems="center">
-        {
-          // Current items
-          items.map(item => {
-            const name = field.getItemName?field.getItemName(item)
-                        :item.id;
-            return <Chip key={item.id} label={name}
-                         variant="outlined"
-                         sx={{ marginRight: "8px",
-                               marginBottom: "8px" }}
-                         onDelete={editable?
-                                   () => deleteArrayItem(field, item)
-                                        :undefined}
-            />
-          })
-        }
+      return <Stack direction="column" spacing={2}>
+        <Stack direction="row" spacing={0} flexWrap="wrap"
+               justifyContent="flex-start" alignItems="center">
+          {
+            // Current items
+            items.map(item => {
+              const name = field.getItemName?field.getItemName(item)
+                          :item.id;
+              return <Chip key={item.id} label={name}
+                           variant="outlined"
+                           sx={{ marginRight: "8px",
+                                 marginBottom: "8px" }}
+                           onDelete={editable?
+                                     () => deleteArrayItem(field, item)
+                                          :undefined}
+              />
+            })
+          }
+        </Stack>
         {
           // Selector to add new ones
           editable && unusedItems.length > 0 &&
-          <Stack direction="row">
-            <IconButton disabled>
-              <Icon>add</Icon>
-            </IconButton>
-            <Select variant="standard">
-              {
-                unusedItems.map((item, i) => {
-                  const name = field.getItemName?
-                               field.getItemName(item):item.id;
-                  return <MenuItem key={i}
-                                   onClick={() => addArrayItem(field, item)}>
-                    {name}
-                  </MenuItem>
-                })
-              }
-            </Select>
-          </Stack>
+          <Autocomplete
+            blurOnSelect={true}
+            getOptionLabel={ item => field.getItemName?field.getItemName(item)
+                                    :String(item.id) }
+            options={unusedItems}
+            renderInput={(params) => <TextField {...params} fullWidth
+                                                label="Add" />}
+            onChange={ (e, value) =>
+              { if (value) addArrayItem(field, value); } }
+          />
         }
       </Stack>
     };
